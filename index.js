1a64d6dd3dcf0cc26f02b14c710c8e65d9cc0720
Scott Hackman <scottbert85@yahoo.com>
2020-03-05 12:14:40 -0500

    First routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 const keys = require("./config/keys");
 
+const hotels=require('./routes/api/hotels');
+
 const app = express();
 
 app.use(express.json());
@@ -15,6 +17,7 @@ mongoose
   .catch(error => console.log("DB Connection error", error));
 
 app.use(express.static("public"));
+app.use('/api/hotels', hotels)
 
 app.listen(3000, () =>
   console.log("listening on port 3000 - KimPossible Operational")
