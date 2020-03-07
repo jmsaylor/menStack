62e93bfc44b8ee79564723a3a83325bd1b316e66
John <johnmsaylor@gmail.com>
2020-03-07 06:27:46 -0500

    post and get routes

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -14,13 +14,22 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   console.log(req.body);
-
   try {
-    let hotelRoom = new Hotel({
-      name: req.body.name
+    let hotel = new Hotel({
+      name: req.body.name,
+      location: req.body.location,
+      rooms: [
+        {
+          roomNumber: req.body.room,
+          roomType: req.body.roomType,
+          roomPrice: req.body.roomPrice,
+          available: req.body.available
+        }
+      ]
     });
-    await hotelRoom.save();
-    res.send(hotelRoom);
+
+    hotel = await hotel.save();
+    res.send(hotel);
   } catch (error) {
     console.error(error);
   }
