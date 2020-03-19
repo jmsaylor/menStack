acb74bd403d8662c60b2758ea68ab1af4400c103
John <johnmsaylor@gmail.com>
2020-03-19 11:50:43 -0400

    fixed reservations routes

--- routes/api/reservations.js
+++ routes/api/reservations.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Hotel = require("../../models/Hotel");
-// const Room = require('../../models/Room') //testing
 
-//POST route for adding reservations - finds hotel, then specific room, and pushes reservation details
-// to array
-router.post("/", async (req, res) => {
+//POST route for adding reservations
+router.post("/:hotelId/:roomId", async (req, res) => {
   console.log("KP Booking - Attempting to make reservation...");
   try {
-    let hotel = await Hotel.findById(req.body.hotelId);
+    let hotel = await Hotel.findById(req.params.hotelId);
     for (let x = 0; x < hotel.rooms.length; x++) {
-      if (hotel.rooms[x]._id == req.body.roomId) {
+      if (hotel.rooms[x]._id == req.params.roomId) {
         var selected = hotel.rooms[x];
         hotel.rooms[x].reservations.push({
           checkIn: req.body.checkIn,
@@ -25,45 +23,46 @@ router.post("/", async (req, res) => {
   }
 });
 
-// Base GET route - Gives all rooms in a hotel without any filters (i.e. - availability, roomType, ...)
-router.get("/:id", async (req, res) => {
+//GET route for all rooms & reservations in a single hotel
+router.get("/:hotelId", async (req, res) => {
   try {
-    let hotel = await Hotel.findById(req.params.id);
-    rooms = hotel.rooms;
-    res.send(rooms);
+    let hotel = await Hotel.findById(req.params.hotelId);
+    res.json(hotel);
   } catch (err) {
     res.status(300).json({ msg: err.message });
   }
 });
 
-// GET routes that takes hotelId and roomId in body to give all reservations for a given room
-router.get("/", async (req, res) => {
-  let hotel = await Hotel.findById(req.body.hotelId);
+//GET route for reservations for a specific room
+router.get("/:hotelId/:roomId", async (req, res) => {
+  let hotel = await Hotel.findById(req.params.hotelId);
   for (let x = 0; x < hotel.rooms.length; x++) {
-    if (hotel.rooms[x]._id == req.body.roomId) {
+    if (hotel.rooms[x]._id == req.params.roomId) {
       var selected = hotel.rooms[x];
     }
   }
   res.json(selected);
 });
 
-//This DELETE route needs 3 things to come through body: hotelId, roomId, & resId
-router.delete("/", async (req, res) => {
+//DELETE route
+router.delete("/:hotelId/:roomId/:resId", async (req, res) => {
   try {
-    let hotel = await Hotel.findById(req.body.hotelId);
+    let hotel = await Hotel.findById(req.params.hotelId);
     for (let x = 0; x < hotel.rooms.length; x++) {
-      if (hotel.rooms[x]._id == req.body.roomId) {
+      if (hotel.rooms[x]._id == req.params.roomId) {
         for (let i = 0; i < hotel.rooms[x].reservations.length; i++) {
-          if (hotel.rooms[x].reservations[i]._id == req.body.resId) {
+          if (hotel.rooms[x].reservations[i]._id == req.params.resId) {
+            console.log("Deleting reservation...");
             console.log(hotel.rooms[x].reservations[i]);
+            var roomSelected = hotel.rooms[x];
             hotel.rooms[x].reservations.splice(i, 1);
-            // var test = hotel.rooms[x].reservations[i];
           }
         }
       }
     }
     await hotel.save();
-    res.json({ msg: "KP Booking - Deleted Reservation" });
+    // res.json({ msg: "KP Booking - Deleted Reservation" });
+    res.json(roomSelected);
   } catch (err) {
     res.status(300).json({ msg: err.message });
   }
